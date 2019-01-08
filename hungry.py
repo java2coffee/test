f230897d69837374421856a541464afa6d06cf82
ZHEYLI <ZHEYLI@ZHEYLI-CN.cn.oracle.com>
2019-01-08 21:11:23 +0800

    water

--- hungry.py
+++ hungry.py
@@ -4,4 +4,7 @@ if hungry == "yes":
     print("eat pizza")
     print("eat burger")
 else:
+    thirsty = input("Are you thirsty?")
+    if thirsty == "yes":
+        print("drink water")
     print("do your homework")
